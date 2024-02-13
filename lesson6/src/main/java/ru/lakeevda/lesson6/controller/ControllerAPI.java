b1ab5817a3d895e4db7c20f981a750b17c9b894f
LakeevDA <ivvi04@gmail.com>
2024-02-13 17:09:26 +0300

    Выполнено домашнее задание по уроку 6

--- lesson6/src/main/java/ru/lakeevda/lesson6/controller/ControllerAPI.java
+++ lesson6/src/main/java/ru/lakeevda/lesson6/controller/ControllerAPI.java
@@ -1,7 +1,6 @@
 package ru.lakeevda.lesson6.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
